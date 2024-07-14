bad7f6f9dca4395fd7fddffae5028cee6bb2f3cd
hayamaster <dnfxmfkdbzos@naver.com>
2024-07-14 22:09:14 +0900

    feat: tab 디자인 수정

--- src/app/pages/[pageId]/page.tsx
+++ src/app/pages/[pageId]/page.tsx
@@ -1,6 +1,7 @@
 // "use client";
 // import { useState, useEffect } from "react";
 import { supabase } from "@/apis";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 interface Data {
   title: string;
@@ -29,13 +30,36 @@ async function getData(pageId: string): Promise<Data | null> {
 
 export default async function Page({ params }: { params: { pageId: string } }) {
   const data = await getData(params.pageId);
+  console.log(data);
 
   return (
-    <main>
-      <h1>{data?.title}</h1>
+    <main className="h-full flex flex-col">
+      <Tabs defaultValue="account" className="w-full h-full p-0">
+        <TabsList className="flex mt-6 w-screen px-0 py-0 bg-transparent">
+          <div className="w-full h-full flex relative">
+            <TabsTrigger
+              value="account"
+              className="absolute left-0 w-[50.8%] m-0 py-2.5 text-gray-200 data-[state=active]:text-blue data-[state=active]:z-20 rounded-t-xl shadow-[0_-1.5px_3px_0px_rgba(0,0,0,0.05)] data-[state=active]:shadow-[3px_-4px_6px_-1px_rgba(0,0,0,0.15)]"
+            >
+              <h1 className="">미팅 전체보기</h1>
+            </TabsTrigger>
+            <TabsTrigger
+              value="password"
+              className="absolute right-0 w-[50.8%] m-0 py-2.5 text-gray-200 data-[state=active]:text-blue data-[state=active]:z-20 rounded-t-xl shadow-[0_-1.5px_3px_0px_rgba(0,0,0,0.05)] data-[state=active]:shadow-[-3px_-4px_6px_-1px_rgba(0,0,0,0.15)]"
+            >
+              <h1 className="">겹치는 시간 확인</h1>
+            </TabsTrigger>
+          </div>
+        </TabsList>
+        <TabsContent value="account">
+          <h1>{data?.title}</h1>
+        </TabsContent>
+        <TabsContent value="password">
+          <h1>{data?.title}</h1>
+        </TabsContent>
+      </Tabs>
 
-      <div>Page</div>
-      <div>siv</div>
+      <h1>{data?.days}</h1>
     </main>
   );
 }
