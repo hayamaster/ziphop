366cdcf5145aac429bf9890807d591c7bca2de08
hayamaster <dnfxmfkdbzos@naver.com>
2024-08-07 16:17:44 +0900

    chore: 리액트 쿼리 설치 및 세팅

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -1,1 +1,2 @@
 export * from "./supabaseClient";
+export { default as ReactQueryClientProvider } from "./reactQueryClientProvider";
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { ReactQueryClientProvider } from "@/apis";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,12 +16,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="w-full h-dvh overflow-hidden">
-      <body className={`${inter.className} w-full h-full`}>
-        <main className="flex w-full h-full items-center justify-center px-10 tablet:mx-auto tablet:max-w-[400px]">
+    <ReactQueryClientProvider>
+      <html lang="en" className="w-full h-dvh overflow-hidden">
+        <body
+          className={`${inter.className} w-full h-full flex items-center justify-center px-10 tablet:mx-auto tablet:max-w-[400px]`}
+        >
           {children}
-        </main>
-      </body>
-    </html>
+        </body>
+      </html>
+    </ReactQueryClientProvider>
   );
 }
